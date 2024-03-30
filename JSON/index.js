9606d9b4c47ea0ad1bac63258c1541c00ab89bb0
Sadik Saroar <91769816+Sadiksaroar@users.noreply.github.com>
2024-03-31 01:25:34 +0600

    Add files via upload

---
+++ JSON/index.js
@@ -0,0 +1,18 @@
+/* How to :
+Access:
+Mdify:
+Delate:
+Convert
+
+json Code
+*/
+// access json code ...
+const myModule = require('./frends_data.json');
+//console.log(myModule);
+
+//console.log(myModule.friends[0]);
+console.log(myModule.friends[0].name);
+
+console.log(myModule.friends[0].name);
+
+
